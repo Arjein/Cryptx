32d6cad30bd134be9ab0f1166a541f9178d3458d
Mert Arcan <mertarcan8@outlook.com>
2022-11-09 21:40:29 +0300

    Cryptx Initialization

---
+++ cryptx/android/app/src/main/kotlin/com/example/cryptx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cryptx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ cryptx/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cryptx',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cryptx'),
+      ),
+      body: const Center(
+        child: Text('Hello Cryptx'),
+      ),
+    );
+  }
+}
